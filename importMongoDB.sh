b0a25bed2758c086dfce6c90ec2797d44ac33e0d
Łukasz Martyński <lmartyns@sigma.inf.ug.edu.pl>
2012-05-17 00:41:40 +0200

    Baza

--- importMongoDB.sh
+++ importMongoDB.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-couchapp push view.js http://$1:$2/cyclist
-curl 'http://'$1':'$2'/cyclist/_design/app/_list/to_json/view' | sed -e 's/},/&\n/g;s/\[//g;s/\]//g;s/},/}/g' > disasters.json
+mongoimport --host localhost --port $1 --db test --collection cyclingteam --type json --file cyclingteam.json
